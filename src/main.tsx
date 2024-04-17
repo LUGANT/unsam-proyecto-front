7f0605f4d4ffff4995c29d569fb55501b3d7af79
Nicolas Stebner <nicolas_stebner@hotmail.com>
2024-04-17 12:19:22 -0300

    entorno terminado

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
   </ChakraProvider>
 )
