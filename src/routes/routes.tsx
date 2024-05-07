4b6ff9553cfcf332ffc8dbd756d83ef11ecb09f3
rtartaglia00 <rtartaglia@estudiantes.unsam.edu.ar>
2024-05-07 03:07:10 -0300

    newevent component

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,7 @@ import { HomePage } from "../pages/home";
 import { LoginPage } from "../pages/login";
 import { AuthLayout } from "../pages/layouts/auth-layout";
 import { MainLayout } from "../pages/layouts/main-layout";
+import NewEventModal from './../components/NewEventModal';
 
 export function PrincipalRoutes() {
   return (
@@ -11,7 +12,7 @@ export function PrincipalRoutes() {
         <Route path = "/auth" element = { <AuthLayout /> }>
             <Route path = "/auth/login" element = { <LoginPage /> }/>
         </Route>
-        <Route path = "/" element = { <MainLayout /> }>
+        <Route path = "/" element = { <NewEventModal /> }>
           <Route path = "/home" element = { < HomePage/> }/>
         </Route>
       </Routes>
