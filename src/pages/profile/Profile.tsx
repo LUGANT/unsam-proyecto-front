b79b19c4ab81693c7134230bf50299ee1512fda6
rtartaglia00 <rtartaglia@estudiantes.unsam.edu.ar>
2024-05-31 16:10:51 -0300

    agregado modal editar datos

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,17 +1,47 @@
 import { StarIcon } from "@chakra-ui/icons"
-import { Box, Flex, HStack, Icon, Image, Text, VStack } from "@chakra-ui/react"
 import { RiPencilFill } from "react-icons/ri";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    useDisclosure,
+    FormControl,
+    FormLabel,
+    Box,
+    Flex, 
+    HStack, 
+    Icon, 
+    Image, 
+    Text, 
+    VStack,
+    Input
+} from '@chakra-ui/react'
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 function Profile() {
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const { username } = useParams();
+
+    const [usuario, setUsuario] = useState(username)
+
+
     return (
         <Flex flexDirection={'column'} flex={'1'}>
             <Flex flexGrow={'1'} bg={'gray.200'} alignItems={'center'} justifyContent={'center'} gap={'20px'}>
                 <Image src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png" boxSize={'200px'} objectFit={'cover'} borderRadius={'full'} />
                 <Box>
-                    <Text fontSize={'3xl'} fontWeight={'semibold'}>Jose1371</Text>
+                    <Text fontSize={'3xl'} fontWeight={'semibold'}>{usuario}</Text>
                     <Text fontSize={'xl'}>Jose Lopez</Text>
                 </Box>
-            <Icon as={RiPencilFill} boxSize={'35px'}/>
+                <Icon as={RiPencilFill} boxSize={'35px'} onClick={onOpen} cursor={'pointer'}/>
             </Flex>
             <Flex flexGrow={'4'} flexDirection={'column'} alignItems={'center'}>
                 <Text fontSize={'xl'} fontWeight={'semibold'} my={'50px'}>Ultimas reseñas</Text>
@@ -21,6 +51,7 @@ function Profile() {
                     <ReviewMiniCard />
                 </VStack>
             </Flex>
+            <EditProfile isOpen={isOpen} onClose={onClose} usuario={usuario}/>
         </Flex>
     )
 }
@@ -52,5 +83,35 @@ const ReviewMiniCard = () => {
                 <Text>Un capo!</Text>
             </Flex>
         </Flex>
+    )
+}
+
+const EditProfile = ({ isOpen, onClose, usuario }) => {
+
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose} size={"sm"}>
+            <ModalOverlay />
+            <ModalContent height={"auto"} display={'flex'} justifyContent={'center'} alignItems={'center'} m={'auto'}>
+                <ModalHeader fontSize={'2xl'} textAlign={'center'} w={'full'}>Editar mis datos</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody display={'flex'} flexDirection={'column'} w={'full'}>
+                    <Flex direction='column' gap={'1rem'} flex={'1'} alignItems={'center'} w={'full'}>
+                        <Image boxSize={'200px'} objectFit={'cover'} borderRadius={'full'} src='https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png'></Image>
+                        <FormControl flex={'1'} display={'flex'} flexDirection={'column'} mt={'20px'}>
+                            <FormLabel fontSize={'xs'} opacity={'.5'}>Usuario</FormLabel>
+                            <Input defaultValue={usuario}></Input>
+                        </FormControl>
+                    </Flex>
+                </ModalBody>
+
+                <ModalFooter alignSelf={'flex-end'}>
+                    <Button variant='ghost' color='brand.300' mr={3} onClick={onClose}>
+                        CANCELAR
+                    </Button>
+                    <Button _hover={{ bg: 'purple.400' }} bg='brand.300' color='white' onClick={onClose}>GUARDAR</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
     )
 }
