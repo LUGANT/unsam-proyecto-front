e82c41837b1d4e98f15e9115b72a683c36e320a4
Gonzalo <gon.mendez13@gmail.com>
2024-06-26 12:48:28 -0300

    protected routes, boton unirse ahora redirige a login cuando nohay sesion, login y signup con rutas de react, opcion de continuar sin ingresar

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AboutPage } from "../pages/about";
 import { ChangePasswordPage } from "../pages/change-password";
@@ -13,6 +14,7 @@ import { useAuth } from "../providers/auth/AuthContext";
 import Profile from "../pages/profile/Profile";
 import { SearchPage } from "../pages/search";
 import { SignUpPage } from "../pages/signup";
+import { ProtectedRoute } from "./protectedRoute";
 
 export function PrincipalRoutes() {
   const { isLoggedIn } = useAuth();
@@ -20,24 +22,29 @@ export function PrincipalRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={ isLoggedIn ? <Navigate to='/'/> : <AuthLayout />}>
+        <Route
+          path="/auth"
+          element={isLoggedIn ? <Navigate to="/" /> : <AuthLayout />}
+        >
           <Route path="/auth/login" element={<LoginPage />} />
           <Route path="/auth/signup" element={<SignUpPage />} />
         </Route>
         <Route path="/" element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/mis-eventos" element={<MisEventos />} />
-          <Route path="/buscar-eventos/:search" element={<SearchPage />} />
-          <Route path="/profile/:username" element={<Profile />} />
-          <Route
-            path="/profile/change-password"
-            element={<ChangePasswordPage />}
-          />
-          <Route path="/evento/:idEvento" element={<EventoDetail />} />
+          <Route element={<ProtectedRoute isLoggedIn={isLoggedIn} />}>
+            <Route path="/mis-eventos" element={<MisEventos />} />
+            <Route path="/buscar-eventos/:search" element={<SearchPage />} />
+            <Route path="/profile/:username" element={<Profile />} />
+            <Route
+              path="/profile/change-password"
+              element={<ChangePasswordPage />}
+            />
+            <Route path="/evento/:idEvento" element={<EventoDetail />} />
+          </Route>
         </Route>
-        <Route path="/404" element={<NotFound />}></Route>
-        <Route path="*" element={<PageNotFound />}></Route>
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
