cfe3cd0a536829f59e041375f51cf74c79d471bf
lugant <lugant.development@gmail.com>
2024-07-17 21:48:56 -0300

    estilos finales chat

--- src/features/chat-events/Message.tsx
+++ src/features/chat-events/Message.tsx
@@ -13,10 +13,6 @@ function Message ({message, index, previousMessage}: {message: ChatMessage, inde
         return (previousMessage) ? previousMessage.usuarioId == message.usuarioId : false
     }
 
-    const sameHour = () => {
-        return timeSplitParam(message.hora) === timeSplitParam(previousMessage?.hora)
-    }
-
     const timeSplitParam = (hora: string) => {
         if (hora === undefined)
             return undefined
@@ -26,29 +22,31 @@ function Message ({message, index, previousMessage}: {message: ChatMessage, inde
 
     return (
         <>
-            {sameUser() ? <SenderMessage message={message} index={index} sameHour={sameHour()}/> : <RecieverMessage message={message} disabledName={sameUserPrevMessage()} index={index} sameHour={sameHour()}/>}
+            {sameUser() ? <SenderMessage message={message} index={index} disableStyle={sameUserPrevMessage()}/> : <RecieverMessage message={message} disabledName={sameUserPrevMessage()} index={index}/>}
         </>            
     )
 }
 
-function SenderMessage({message, index, sameHour}: {message: ChatMessage, index: number, sameHour: boolean}){
+function SenderMessage({message, index, disableStyle}: {message: ChatMessage, index: number, disableStyle: boolean}){
     return( 
-    <Box marginBlock={"2px"} bg={"#ddd"} padding={'10px'} rounded={"10px 0 10px 10px"} key={index} alignSelf={'flex-end'}>
+    <Box display={'flex'} marginBlock={"2px"} gap={'10px'} bg={"#ddd"} padding={'10px'} rounded={disableStyle ? "10px" :"10px 0 10px 10px"} key={index} alignSelf={'flex-end'}>
         <Text>{message.texto}</Text>
-        {sameHour ? '' : <Text fontSize={'2xs'}>{message.hora}</Text>}
+        <Text display={'flex'} flexDirection={'column'} justifyContent={'flex-end'} fontSize={'2xs'}>{message.hora}</Text>
     </Box>
     )
 }
 
-function RecieverMessage({message, index, disabledName, sameHour}: {message: ChatMessage, index: number, disabledName: boolean, sameHour: boolean}){
+function RecieverMessage({message, index, disabledName}: {message: ChatMessage, index: number, disabledName: boolean}){
     
     return (
     <Box display={"flex"} gap={"10px"} paddingLeft={disabledName ? '40px' : '' }>
         {disabledName ? "" : <Avatar src={message.imgUrl} size={"sm"} bg={"brand.300"}/>}
-        <Box marginBlock={"2px"} bg={"#c29aff"} padding={'10px'} rounded={disabledName ? "10px" : "0 10px 10px 10px"} key={index} alignSelf={'flex-start'}>
-            {disabledName ? "" : <Text fontWeight={"bold"}>{message.username}</Text>}
-            <Text>{message.texto}</Text>
-            {sameHour ? '' : <Text fontSize={'2xs'}>{message.hora}</Text>}
+        <Box display={'flex'} gap={'10px'} marginBlock={"2px"} bg={"#c29aff"} padding={'10px'} rounded={disabledName ? "10px" : "0 10px 10px 10px"} key={index} alignSelf={'flex-start'}>
+            <Box>
+                {disabledName ? "" : <Text fontWeight={"bold"}>{message.username}</Text>}
+                <Text>{message.texto}</Text>
+            </Box>
+            <Text display={'flex'} flexDirection={'column'} justifyContent={'flex-end'} fontSize={'2xs'}>{message.hora}</Text>
         </Box>
     </Box>
     )
